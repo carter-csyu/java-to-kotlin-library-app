82ed089130346163abf43e3f1a10618a03d05aa7
chunsang.yu <chunsang.yu@gmail.com>
2022-11-15 23:36:09 +0900

    feat: lec04 뺄샘, 곱샘, 나눗셈 테스트코드 추가

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Assertions.*
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
 }
 
 class CalculatorTest {
@@ -18,6 +21,45 @@ class CalculatorTest {
 
         // then
         if (calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(5)
+
+        // then
+        if (calculator.number != 1) {
             throw IllegalStateException()
         }
     }
