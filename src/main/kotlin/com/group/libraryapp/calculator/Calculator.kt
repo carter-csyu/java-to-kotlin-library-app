5c211be4c0a5a321137b6d129203a18f2403c1e9
chunsang.yu <chunsang.yu@gmail.com>
2022-11-15 23:30:04 +0900

    feat: lec04 클래스 public field를 이용한 테스트코드 작성

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -1,22 +1,19 @@
 package com.group.libraryapp.calculator
 
 class Calculator(
-    private var _number: Int,
+    var number: Int,
 ) {
 
-    val number: Int
-        get() = this._number
-
     fun add(operand: Int) {
-        this._number += operand
+        this.number += operand
     }
 
     fun minus(operand: Int) {
-        this._number -= operand
+        this.number -= operand
     }
 
     fun multiply(operand: Int) {
-        this._number *= operand
+        this.number *= operand
     }
 
     fun divide(operand: Int) {
@@ -24,6 +21,6 @@ class Calculator(
             throw IllegalArgumentException("0으로 나눌 수 없습니다.")
         }
 
-        this._number /= operand
+        this.number /= operand
     }
 }
